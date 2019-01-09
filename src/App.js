42cd3db93bf6a9d44050ba1c91f905f10bdb700a
Kevin Parine <kparine314@gmail.com>
2019-01-09 08:50:32 -0800

    Add Search Fn

--- src/App.js
+++ src/App.js
@@ -5,6 +5,28 @@ import BookList from './BookList';
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+
+    handleChange = (e) => {
+      this.setState({
+        [e.target.name]: [e.target.value]
+      })
+    }
+  
+    searchBook = (ele) => {
+      const normTitle = ele.title.toLowerCase()
+      const normAuthor = ele.author.toLowerCase()
+      const normSearchString = this.state.searchString.toLowerCase()
+
+      return normTitle.includes(normSearchString) || normAuthor.includes(normSearchString)
+    }
+    byColumns = (a, b) => {
+      return a[this.state.sortBy] > b[this.state.sortBy] ? 1 : -1
+    }
+  
+  }
 
   render() {
     return (
